d731153114452c542572edfad74a91e6482af0b0
Kathleen <kathleenmcclay@gmail.com>
2017-08-09 14:20:03 -0400

    All 4 links working with responding data types - html, text, JSON, CSV

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
 
+    respond_to do |format|
+      format.html
+      format.text
+
+      format.json do
+        render json: @books
+      end
+
+      format.csv do
+        render plain: @books
+      end
+    end
+  end
 end
